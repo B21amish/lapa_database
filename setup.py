4ebca708ce2a8a3b9c91845a4efbeeaae8c2cd7f
Parth Mangtani <impmmmm@gmail.com>
2024-01-25 22:31:31 +0530

    change psycopg2 to psycopg2-binary

--- setup.py
+++ setup.py
@@ -11,16 +11,16 @@ setup(
     },
     install_requires=[
         "sqlalchemy>=2.0.23",
-        "psycopg2>=2.9.9",
+        "psycopg2-binary>=2.9.9",
         "uvicorn>=0.24.0.post1",
         "fastapi>=0.104.1",
         "python-multipart>=0.0.6",
         "square_logger~=1.0",
-        "websockets>=12.0"
+        "websockets>=12.0",
     ],
     extras_require={
-        'all': [
-            'database_structure~=0.0.1',
+        "all": [
+            "database_structure~=0.0.1",
         ],
     },
     author="thePmSquare, Amish Palkar, Lav Sharma",
